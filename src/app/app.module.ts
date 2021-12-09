5cc3cf9e47ee740729ae84e53a8bf44a70064473
A Rodrigues Da Silva <alejandro-abel.rodrigues-da-silva@capgemini.com>
2021-12-09 13:10:07 +0100

    Angular task1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MonthsComponent } from './months/months.component';
+import { DaysComponent } from './days/days.component';
+import { FlowersComponent } from './flowers/flowers.component';
+import { TownsComponent } from './towns/towns.component';
+import { FoodsComponent } from './foods/foods.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonthsComponent,
+    DaysComponent,
+    FlowersComponent,
+    TownsComponent,
+    FoodsComponent
   ],
   imports: [
     BrowserModule
